ee04385d2039f9a36e2fe8d18c08b4112f192057
auroravo2001 <vo23d@mtholyoke.edu>
2021-07-09 18:42:58 +0700

    feat: homepage feed v1
    the overall idea of how the homepage would look - it still needs a search bar; categories filter; sorting dropdown; interactive buttons to like; etc.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,71 @@
-// import {useState} from 'react';
-// import ReviewList from './ReviewList';
+import {useState} from 'react';
+import ReviewList from './ReviewList';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Fab from '@material-ui/core/Fab';
+
 
 /**
  * Homepage
  * 
  */
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+}));
 
-function Home() {
+function ScrollTop(props) {
+    const {children} = props;
+    const classes = useStyles();
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (e) => {
+        const anchor = (e.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            window.scrollTo({top: 0, behavior: 'smooth', block: 'center'});
+        }
+    };
+        
     return (
-        <div className="home">
-            <h2> Homepage </h2>
-        </div>
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={classes.root}>
+                {children}
+            </div>
+        </Zoom>
     );
 }
 
-export default Home;
+export default function Home(props) {
+    /* examples */
+    const [reviews] = useState([
+        {name: 'Alice', bookname: 'Book Name #1', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', categories: 'Fantasy, Sci-fi', id: 1},
+        {name: 'Bob', bookname: 'Book Name #2', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', categories: 'Romance' , id: 2},
+        {name: 'Charles', bookname: 'Book Name #3', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', categories: 'Nonfiction, Science' , id: 3},
+    ]);
+
+    return (
+        <div className="home">
+            <div className="top">
+            </div>
+            <div className="body">
+                <ReviewList reviews = {reviews}/>
+            </div>
+            <Toolbar id="back-to-top-anchor" />
+                <ScrollTop {...props}>
+                    <Fab size="small" aria-label="scroll to top">
+                        <KeyboardArrowUpIcon />
+                    </Fab>
+                </ScrollTop>
+        </div>
+    );
+}
--- client/src/ReviewList.js
+++ client/src/ReviewList.js
@@ -1,3 +1,6 @@
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+// import FavoriteIcon from '@material-ui/icons/Favorite'; 
+
 function ReviewList (prop) {
 
     const reviews = prop.reviews;
@@ -6,7 +9,13 @@ function ReviewList (prop) {
         <div className="review-list">
             {reviews.map((review) => (
                 <div className="review-preview" key={review.id}>
-                    <h2> Published by {review.author} </h2>
+                    <h1> {review.bookname} </h1>
+                    <h2> Published by {review.name} </h2>
+                    <p> {review.body} </p>
+                    <div className="footer">
+                        <p> {review.categories} </p>
+                        <FavoriteBorderIcon className="footer-right"/>
+                    </div>
                 </div>
             ))}
         </div>
