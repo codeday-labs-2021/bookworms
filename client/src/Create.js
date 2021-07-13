018f481e7a2d15993930aba72efebc9bcde9549f
auroravo2001 <vo23d@mtholyoke.edu>
2021-07-13 17:58:05 +0700

    feat: checking for blanks in create fields
    warn users if they accidentally leave a field blank

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,8 +1,9 @@
-// import {useState} from 'react';
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {useHistory} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './css/create.module.css';
 
@@ -22,51 +23,80 @@ function Create () {
     /* other page related components */
     // const [isPending, setIsPending] = useState(false);
     const history = useHistory();
+    const [validated, setValidated] = useState(false);
 
     function handleSubmit(e) {
-        // e.preventDefault();
-        // setIsPending(true);
+        const form = e.currentTarget;
+        if (form.checkValidity() === false){
+            e.preventDefault();
+            e.stopPropagation();
+        } else {
+            e.preventDefault();
+            history.push('/');
+        }
 
-        // setIsPending(false);
-        // history.push('/');
+        setValidated(true);
     }
 
     return (
         <div className={styles.create}>
             <h2 className={styles.heading}> Add a New Book Review </h2>
 
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group as={Row} controlId="formName">
                     <Form.Label column sm="4" className={styles.label}>Name</Form.Label>
                     <Col sm="8">
-                    <Form.Control required type="text" className={styles.inputArea}/>
+                    <Form.Control 
+                        required 
+                        type="text" 
+                        className={styles.inputArea}/>
                     </Col>
+                    <Form.Control.Feedback type="invalid">
+                        Please provide your name.
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group as={Row} controlId="formBookName">
                     <Form.Label column sm="4" className={styles.label}>Book Name</Form.Label>
                     <Col sm="8">
-                    <Form.Control type="text" className={styles.inputArea}/>
+                    <Form.Control 
+                        required
+                        type="text" 
+                        className={styles.inputArea}/>
                     </Col>
+                    <Form.Control.Feedback type="invalid">
+                        Please provide the book name.
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group as={Row} controlId="formReview">
                     <Form.Label column sm="4" className={styles.label}>Book Review</Form.Label>
                     <Col sm="8">
-                    <Form.Control as="textarea" rows={3} className={styles.inputArea}/>
+                    <Form.Control 
+                        as="textarea" 
+                        rows={3} 
+                        className={styles.inputArea}
+                        required/>
                     </Col>
+                    <Form.Control.Feedback type="invalid">
+                        Please provide your thoughts and ideas about the book.
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group as={Row} controlId="formCategory">
                     <Form.Label column sm="4" className={styles.label}>Book Categories</Form.Label>
                     <Col sm="8">
-                    <Form.Control type="text" className={styles.inputArea}/>
+                    <Form.Control 
+                        required
+                        type="text" 
+                        className={styles.inputArea}/>
                     </Col>
+                    <Form.Control.Feedback type="invalid">
+                        Please provide the book categories.
+                    </Form.Control.Feedback>
                 </Form.Group>
-
+                <Button className={styles.submitButton} type="submit"> Submit </Button>
             </Form>
-
-            <button className={styles.submitButton} onClick={handleSubmit}> Submit </button>
         </div>
         
     );
