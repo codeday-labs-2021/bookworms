98cab2660c0bd034d707815cb567c9a35404f189
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-16 07:14:20 +0200

    chore: called content type twice

--- server/utils/headers.go
+++ server/utils/headers.go
@@ -5,6 +5,5 @@ import "net/http"
 func HandleCors(w *http.ResponseWriter, r *http.Request, allowedMethods string) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", allowedMethods)
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
 }
