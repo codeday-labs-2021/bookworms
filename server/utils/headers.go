7f51d2396647b06f37b9b12b442916e9db146cf3
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-30 08:20:17 +0200

    chore: few fixes

--- server/utils/headers.go
+++ server/utils/headers.go
@@ -6,5 +6,6 @@ func HandleCors(w *http.ResponseWriter, allowedMethods string) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", allowedMethods)
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	(*w).Header().Set("Access-Control-Max-Age", "3600")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
